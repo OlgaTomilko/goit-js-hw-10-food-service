4357ab3f9387df9ff9711dde25affc5cf6f8793e
OlgaTomilko <67008325+OlgaTomilko@users.noreply.github.com>
2020-12-12 15:00:43 +0300

    Добавлено функционал изменения темы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './styles.css';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+  body: document.querySelector('body'),
+  toggle: document.querySelector('#theme-switch-toggle'),
+};
+
+refs.toggle.addEventListener('change', onToggle);
+
+onLoad();
+
+function onLoad() {
+  const currentTheme = localStorage.getItem('theme');
+
+  currentTheme
+    ? refs.body.classList.add(currentTheme)
+    : refs.body.classList.add(Theme.LIGHT);
+
+  if (currentTheme === Theme.DARK) refs.toggle.setAttribute('checked', true);
+}
+
+function onlocalStorage() {
+  refs.body.classList.contains(Theme.DARK)
+    ? localStorage.setItem('theme', Theme.DARK)
+    : localStorage.setItem('theme', Theme.LIGHT);
+}
+
+function onToggle() {
+  refs.body.classList.toggle(Theme.DARK);
+  onlocalStorage();
+}
